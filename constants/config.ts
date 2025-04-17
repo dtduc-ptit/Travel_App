c4d723c20df23265e7c5b1721a4d20a3191e2482
Tran Minh Quang <quangteotran213@gmail.com>
2025-04-17 19:57:43 +0700

    Update chuc nang cong dong va chuc nang kien thuc

--- constants/config.ts
+++ constants/config.ts
@@ -2,4 +2,4 @@
 
  export const GOOGLE_MAPS_API_KEY = "AIzaSyBtUSjJM_Fh-_KB4DyKIspZTIU4hgi0zzA";
 
-export const API_BASE_URL = "http://172.18.48.1:8000"; 
+export const API_BASE_URL = "http://192.168.41.61:8000"; 
